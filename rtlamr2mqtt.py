38943e9ec28dc27dc42fc0f80e90aa9e3dd27aff
Dave Naumann <davidanaumann@yahoo.com>
2021-09-01 01:01:24 +0000

    Remove autodiscovery logic. Remove mqtt authentication. Fix bug in mqtt topic

--- rtlamr2mqtt.py
+++ rtlamr2mqtt.py
@@ -37,18 +37,8 @@ with open('/etc/rtlamr2mqtt.yaml','r') as config_file:
 # Build MQTT configuration
 mqtt_host = "127.0.0.1" if 'host' not in config['mqtt'] else config['mqtt']['host']
 mqtt_port = 1883 if 'port' not in config['mqtt'] else int(config['mqtt']['port'])
-mqtt_user = "user" if 'user' not in config['mqtt'] else config['mqtt']['user']
-mqtt_password = "secret" if 'password' not in config['mqtt'] else config['mqtt']['password']
-ha_autodiscovery_topic = "homeassistant" if 'ha_autodiscovery_topic' not in config['mqtt'] else config['mqtt']['password']
-ha_autodiscovery = False
-if 'ha_autodiscovery' in config['mqtt']:
-    if str(config['mqtt']['ha_autodiscovery']).lower() in ['true', 'yes']:
-        ha_autodiscovery = True
-ha_autodiscovery = False if 'ha_autodiscovery' not in config['mqtt'] else config['mqtt']['ha_autodiscovery']
-state_topic = 'rtlamr/{}/state'
-discover_topic = 'homeassistant/sensor/rtlamr/{}/config'
+state_topic = '/rtlamr/{}/state'
 mqtt_client = mqtt.Client(client_id='rtlamr2mqtt')
-mqtt_client.username_pw_set(username=mqtt_user, password=mqtt_password)
 
 if 'general' in config:
     sleep_for = 0 if 'sleep_for' not in config['general'] else config['general']['sleep_for']
@@ -56,7 +46,6 @@ else:
     sleep_for = 0
 
 # Build RTLAMR config
-# TODO: Add a configuration section for rtlamr and rtl_tcp configuration parameters
 protocols = []
 meter_ids = []
 meter_readings = {}
@@ -67,18 +56,6 @@ for idx,meter in enumerate(config['meters']):
     protocols.append(meter['protocol'])
     meter_ids.append(str(meter['id']))
     meter_readings[str(meter['id'])] = 0
-    # if HA Autodiscovery is enabled, send the MQTT payload
-    if ha_autodiscovery:
-        print('Sending MQTT autodiscovery payload to Home Assistant...', file=sys.stderr)
-        discover_payload = {
-            "name": config['meters'][idx]['name'],
-            "unit_of_measurement": config['meters'][idx]['unit_of_measurement'],
-            "icon": config['meters'][idx]['icon'],
-            "state_topic": state_topic.format(config['meters'][idx]['name'])
-        }
-        mqtt_client.connect(host=mqtt_host, port=mqtt_port)
-        mqtt_client.publish(topic=discover_topic.format(config['meters'][idx]['name']), payload=dumps(discover_payload), qos=0, retain=True)
-        mqtt_client.disconnect()
 
 rtlamr_custom = []
 if 'custom_parameters' in config:
